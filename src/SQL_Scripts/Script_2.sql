b780790cfa5035537dd272fbad68d88cafae7cc0
varunTyagarayanG <156878356+varunTyagarayanG@users.noreply.github.com>
2024-12-26 02:46:35 +0530

    Update Script_2.sql

--- src/SQL_Scripts/Script_2.sql
+++ src/SQL_Scripts/Script_2.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS seq_tracker (
     seq_id BIGINT NOT NULL DEFAULT 0
 );
 
--- Insert the shard entry for shard ID 2
+-- Insert the shard entry for shard ID 1
 INSERT INTO seq_tracker (shard_id, seq_id) VALUES (1, 0)
 ON DUPLICATE KEY UPDATE seq_id = seq_id;
